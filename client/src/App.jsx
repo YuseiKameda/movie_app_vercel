ea1fd3f53d99a0fbabc42740b1590cb48d1e43f8
YuseiKameda <yuseibeil5@gmail.com>
2024-10-25 16:28:34 +0900

    検索機能の追加

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,19 @@
-import { useState, useEffect } from 'react'
+import React from "react";
+import MovieSearch from "./components/MovieSearch";
+import Header from "./components/header";
+import Footer from "./components/Footer";
 
-function App() {
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    fetch('http://localhost:3000/api/items')
-      .then(response => response.json())
-      .then(data => setItems(data))
-      .catch(error => console.error('Error feching data', error));
-  }, []);
-
+const App = () => {
   return (
-    <>
-      <div>
-        <h1>Items List</h1>
-        <ul>
-          {items.map(item => (
-            <li key={item.id}>{item.name}</li>
-          ))}
-        </ul>
-      </div>
-    </>
-  )
-}
+    <div>
+      <Header />
+      <main>
+        <h1>Movie Search App</h1>
+        <MovieSearch />
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
-export default App
+export default App;
